d15bee326e63cfe6cd2e0549b974b1e457ce33af
Arowolo Tunmise <tunmisearowolo094@gmail.com>
2025-12-23 09:04:31 +0100

    fixing sidemenu scrool

--- hooks/UseScroll.ts
+++ hooks/UseScroll.ts
@@ -12,9 +12,13 @@ export const useScroll = () => {
     dispatch(setActive(id));
 
     const el = document.getElementById(id);
-    if (el) {
-      el.scrollIntoView({ behavior: "smooth" });
-    };
+    if (!el) return;
+    const yOffset = -56
+    const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth'
+    })
   };
 
   return { handleScroll };
